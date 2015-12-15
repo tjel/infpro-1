b2fb809e4e874b00d541c010cf875e4f5d604eb7
mac <zajec_000@lap>
2015-12-15 09:19:53 +0100

    Dodanie metod dbGetTimeTest() i dbSetTimeTest()

--- src/mathgym/dbOps/dbThreadTest.java
+++ src/mathgym/dbOps/dbThreadTest.java
@@ -12,59 +12,86 @@ import java.sql.SQLException;
  * @author zajec_000
  */
 public class dbThreadTest {
-    
+
     /**
      *
      * @param args
      * @throws ClassNotFoundException
      * @throws SQLException
      */
-    public static void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException{
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException {
         //dbBasics dbConnection = new dbBasics(); //sposób na obiekt
         new dbBasics().start(); //sposób na wątki
         //Thread.sleep(1000000);
         //dbBasics.dbSelect("SELECT * FROM modules");
         //System.out.println(dbBasics.dbSelectCost(2));
-        
+
         //dbBasics.dbUpdadeCost(2, 600);
         //dbBasics.dbSelect("SELECT * FROM modules");
         //System.out.println(dbBasics.dbSelectCost(2));
-        
         //dbCreate.Create_DB();
         //dbBasics.dbUpdateOwned(2,0);
         //System.out.println(dbBasics.dbGetValue(2, "cost"));
         //System.out.println(dbBasics.dbGetSuccessful(2));
         //dbBasics.dbEarnedSuccessful(2);
-        
         //dbBasics.dbInsert(3,"Dodawanie Wielokrotne", 700);
         //dbBasics.dbSelect("SELECT * FROM modules");
-        
         //System.out.println(dbBasics.dbGetSuccessful(2));
-        
-        //dbBasics.dbSelect("SELECT * FROM modules");
-        
-        //dbTimeTest(100);
-        
+        dbBasics.dbSelect("SELECT * FROM modules");
+
+        //dbSelectTimeTest(1000);
+        dbGetTimeTest(1000);
+        dbSetTimeTest(1000);
+
     }
-    
-    
+
     /**
      * Metoda testująca szybkość wykonania zapytania.
-     * 
+     *
      * @param d ilość zapytań
-     * @throws SQLException 
+     * @throws SQLException
      */
-    public static void dbTimeTest(int d) throws SQLException{
+    public static void dbSelectTimeTest(int d) throws SQLException {
         int sum = 0;
-        for(int i = 0; i < d; i++){
-        long startTime = System.nanoTime();
-        dbBasics.dbSelect("SELECT * FROM modules WHERE cost > 650;");
-        long endTime = System.nanoTime();
+        for (int i = 0; i < d; i++) {
+            long startTime = System.nanoTime();
+            dbBasics.dbSelect("SELECT * FROM modules WHERE cost > 650;");
+            long endTime = System.nanoTime();
 
-        long duration = (endTime - startTime)/1000;  //divide by 1000000 to get milliseconds.
-        System.out.println(duration + " microseconds");
-        sum += duration;
+            long duration = (endTime - startTime) / 1000;  //divide by 1000000 to get milliseconds.
+            //System.out.println(duration + " microseconds");
+            sum += duration;
         }
-        System.out.println("Average time: " + sum/d + " microseconds.");
+        System.out.println("Average Select time: " + sum / d + " microseconds.");
+    }
+
+    public static void dbGetTimeTest(int d) {
+        int get = 0;
+        int sum = 0;
+        for (int i = 0; i < d; i++) {
+            long startTime = System.nanoTime();
+            get = dbBasics.dbGetCost(1);
+            long endTime = System.nanoTime();
+
+            long duration = (endTime - startTime) / 1000;  //divide by 1000000 to get milliseconds.
+            //System.out.println(duration + " microseconds");
+            sum += duration;
+        }
+        System.out.println("Average Get time: " + sum / d + " microseconds.");
+    }
+
+    public static void dbSetTimeTest(int d) {
+        int set = 1;
+        int sum = 0;
+        for (int i = 0; i < d; i++) {
+            long startTime = System.nanoTime();
+            dbBasics.dbSetCost(1, 300);
+            long endTime = System.nanoTime();
+
+            long duration = (endTime - startTime) / 1000;  //divide by 1000000 to get milliseconds.
+            //System.out.println(duration + " microseconds");
+            sum += duration;
+        }
+        System.out.println("Average Set time: " + sum / d + " microseconds.");
     }
 }
