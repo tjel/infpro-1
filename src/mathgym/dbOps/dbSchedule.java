ef9460d4826e7081f4a9de12975c6e6c65e5950e
mac <zajec_000@lap>
2015-12-16 13:23:26 +0100

    Utworzenie klasy dbSchedule z zagnieżdżoną klasą dbTaskEvent().  utworzenie konstruktora dbSchedule() tworzącego obiekt klasy Timer() Klasa ma na celu utworzenie wątku, który zapisuje baze danych co konkretny odstęp czasu.

---
+++ src/mathgym/dbOps/dbSchedule.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathgym.dbOps;
+
+import java.util.Timer;
+import java.util.TimerTask;
+/**
+ *
+ * @author zajec_000
+ */
+public class dbSchedule{
+    private static Timer dbTimer;
+    private static final int seconds = 5;
+    
+    /**
+     * Konstruktor tworzący obietk typu Timer następnie wywołujący klase TimerTask.
+     */
+    public dbSchedule(){
+        dbSchedule.dbTimer = new Timer();
+        dbTimer.schedule(new dbTaskEvent(), seconds * 1000);
+    }
+    
+    /**
+     * metoda wykonujaca commit na bazie danych.
+     */
+    private static void dbSaveEvent(){
+        dbBasics.dbTaskCommit();
+        System.out.println("Database save operation successful.");
+    }
+    
+    /**
+     * klasa dbTaskEvent wykonująca co określony czas w konstruktorze metode dbSaveEvent
+     */
+    class dbTaskEvent extends TimerTask {
+    @Override
+    public void run() {
+        dbSaveEvent();
+    }
+  }
+}
